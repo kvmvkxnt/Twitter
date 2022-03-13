09189110554547b0956057a10d2976c223df9c93
kvmvkxnt <kamron1007dada@gmail.com>
2022-03-13 23:05:14 +0500

    Login is nearly done

--- src/Components/Content/Content.jsx
+++ src/Components/Content/Content.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import {Context} from '../../Context/Theme/Theme';
 
 function Content(){
+    const ctx = React.useContext(Context);
     return (
-        <>This is The Main Page <br /> It's under contruction, so please wait some time and enjoy login page! :)</>
+        <p className={`content ${ctx.theme === 'dark' ? 'content--dark' : ''}`}>This is The Main Page <br /> It's under contruction, so please wait some time and enjoy login page! :)</p>
     )
 }
 
--- src/Context/Theme/Theme.jsx
+++ src/Context/Theme/Theme.jsx
@@ -6,6 +6,7 @@ function Provider({ children }) {
     const [theme, setTheme] = React.useState(window.localStorage.getItem('theme') || 'light');
     React.useEffect(() => {
         window.localStorage.setItem('theme', theme);
+        window.document.body.className = `body ${theme === 'dark' ? 'body--dark' : ''}`
     }, [theme]);
 
     return <Context.Provider value={{theme, setTheme}}>{children}</Context.Provider>
