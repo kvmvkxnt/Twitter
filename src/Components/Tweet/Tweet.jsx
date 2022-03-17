2f52b891a697838b9e81c72e0729ec0cc69f9429
kvmvkxnt <kamron1007dada@gmail.com>
2022-03-17 15:52:58 +0500

    Added some css

--- src/Components/Tweet/Tweet.jsx
+++ src/Components/Tweet/Tweet.jsx
@@ -4,22 +4,42 @@ import user1 from '../../Assets/Images/Ellipse3.png';
 import user2 from '../../Assets/Images/Ellipse3@2x.png';
 import TweetIcons from '../Lib/Icons/TweetIcons';
 
-function Tweet({className, name, username, time, tweet}) {
+function Tweet({ className, name, username, time, tweet }) {
     return (
-        <li className={`tweet ${className}__item`}>
-            <a href="#link" className={`${className}__user`}>
+        <li className={`tweet__item ${className}__item`}>
+            <a href="#link" className={`tweet__user ${className}__user`}>
                 <img src={user1} srcSet={`${user1} 1x, ${user2} 2x`} alt="User" />
             </a>
 
-            <div className={`${className}__tweet-info`}>
-                <p className={`${className}__tweet-info__user`}>{name}<span>{username}</span> &bull; <span>{time}</span></p>
-                <p className={`${className}__tweet-info__tweet`}>
+            <div className={`tweet__tweet-info ${className}__tweet-info`}>
+                <a href='#link3' className={`tweet__tweet-info__user ${className}__tweet-info__user`}><span className='name'>{name}cloutexhibition</span> {username}@RajLahoti &bull; {time}25m</a>
+                <p className={`tweet__tweet-info__tweet ${className}__tweet-info__tweet`}>
                     {tweet}
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Similique quas quaerat nam ratione id accusamus, odio iusto, soluta repellendus dolor in doloribus iure quos aliquam? Nemo, quos molestiae? Cupiditate, nulla.
                 </p>
-                <div className={`${className}__tweet-info__buttons`}>
-                    <TweetIcons type='stats' />
+                <div className={`tweet__tweet-info__buttons ${className}__tweet-info__buttons`}>
+                    <button className={`tweet__tweet-info__button ${className}__tweet-info__button`}>
+                        <TweetIcons type='comments' />
+                        5
+                    </button>
+                    <button className={`tweet__tweet-info__button ${className}__tweet-info__button`}>
+                        <TweetIcons type='retweet' />
+                    </button>
+                    <button className={`tweet__tweet-info__button ${className}__tweet-info__button`}>
+                        <TweetIcons type='like' />
+                    </button>
+                    <button className={`tweet__tweet-info__button ${className}__tweet-info__button`}>
+                        <TweetIcons type='share' />
+                    </button>
+                    <button className={`tweet__tweet-info__button ${className}__tweet-info__button`}>
+                        <TweetIcons type='stats' />
+                    </button>
                 </div>
             </div>
+
+            <button className={`tweet__button ${className}__button`}>
+                &bull;&bull;&bull;
+            </button>
         </li>
     )
 }
