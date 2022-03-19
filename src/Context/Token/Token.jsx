ca173e55e917b269f6e6430339be79dd90aa589a
kvmvkxnt <kamron1007dada@gmail.com>
2022-03-19 20:40:56 +0500

    Added proptypes

--- src/Context/Token/Token.jsx
+++ src/Context/Token/Token.jsx
@@ -4,7 +4,11 @@ const Context = React.createContext();
 function Provider({ children }) {
     const [token, setToken] = React.useState(window.localStorage.getItem('token') || null);
     React.useEffect(() => {
-        window.localStorage.setItem('token', token);
+        if (token) {
+            window.localStorage.setItem('token', token);
+        } else {
+            return;
+        }
     }, [token]);
     return <Context.Provider value={{ token, setToken }}>{children}</Context.Provider>
 }
