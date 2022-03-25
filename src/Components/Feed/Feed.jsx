19ee931ab52708a224ed5b973aa920f99ac505e0
kvmvkxnt <kamron1007dada@gmail.com>
2022-03-25 13:48:46 +0500

    Tried to make the website responsive

--- src/Components/Feed/Feed.jsx
+++ src/Components/Feed/Feed.jsx
@@ -56,7 +56,14 @@ function Feed() {
         <div className='feed'>
             <div className="feed__inner">
                 <div className="feed__heading">
-                    <h1 className="feed__title">{languages[ctxLang.lang].main.feed.heading}</h1>
+                    <div className="feed__controls">
+                        <button className="feed__burger">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-list" viewBox="0 0 16 16">
+                                <path fillRule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z" />
+                            </svg>
+                        </button>
+                        <h1 className="feed__title">{languages[ctxLang.lang].main.feed.heading}</h1>
+                    </div>
                     <select className="feed__select" defaultValue={ctxLang.lang} onChange={(evt) => {
                         ctxLang.setLang(evt.target.value);
                     }}>
